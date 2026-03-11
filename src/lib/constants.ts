1d4558a803e314dfd0b4399a794a5b3f5e6c4c1c
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2026-03-11 19:48:10 +0100

    upgrade contracts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const PROGRAM_ID = "veilmarkets_v3.aleo";
-export const TOKEN_PROGRAM_ID = "veilmarkets_token_v3.aleo";
-export const ORACLE_PROGRAM_ID = "veilmarkets_oracle_v3.aleo";
-export const FACTORY_PROGRAM_ID = "veilmarkets_factory_v3.aleo";
+export const PROGRAM_ID = "veilmarkets_v4.aleo";
+export const TOKEN_PROGRAM_ID = "veilmarkets_token_v4.aleo";
+export const ORACLE_PROGRAM_ID = "veilmarkets_oracle_v4.aleo";
+export const FACTORY_PROGRAM_ID = "veilmarkets_factory_v4.aleo";
